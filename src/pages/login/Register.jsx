94d2746c3813c8a917e2e661953b6f15292168b5
dongtiendung <121683417+dongtiendung@users.noreply.github.com>
2024-06-27 10:54:11 +0900

    Update Register.jsx

--- src/pages/login/Register.jsx
+++ src/pages/login/Register.jsx
@@ -26,7 +26,7 @@ function Register() {
         e.preventDefault();
 
         try {
-            const res = await axios.post('https://asm02-nodejs-backend.onrender.com//api/auth/register', {
+            const res = await axios.post('https://asm02-nodejs-backend.onrender.com/api/auth/register', {
                 username: credentials.username,
                 password: credentials.password,
                 fullname: credentials.fullname,
